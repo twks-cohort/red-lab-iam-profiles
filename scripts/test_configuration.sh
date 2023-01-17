a4ae1017a69b0fa3fc4801723a2c77e1c95576c6
Richard Lau <rilau@thoughtworks.com>
2023-01-17 16:55:38 -0500

    Fix failed test setup

--- scripts/test_configuration.sh
+++ scripts/test_configuration.sh
@@ -2,15 +2,15 @@
 export ENVIRONMENT=$1
 export AWS_ACCOUNT_ID=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .account_id)
 
-aws-vault exec tjriceepc --no-session -- aws sts assume-role --output json --role-arn arn:aws:iam::${AWS_ACCOUNT_ID}:role/DPSIamProfilesRole --role-session-name awspec-test > credentials
+aws sts assume-role --output json --role-arn arn:aws:iam::${AWS_ACCOUNT_ID}:role/DPSIamProfilesRole --role-session-name awspec-test > credentials
 
 export AWS_ACCESS_KEY_ID=$(cat credentials | jq -r ".Credentials.AccessKeyId")
 export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAccessKey")
 export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
 export AWS_DEFAULT_REGION=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_region)
 
-inspec exec ../test/iam-roles -t aws://
+inspec exec test/iam-roles -t aws://
 
 if [[ ${ENVIRONMENT} == "nonprod" ]]; then
-  inspec exec ../test/iam-profiles -t aws://
+  inspec exec test/iam-profiles -t aws://
 fi
